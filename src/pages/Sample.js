016b422dce4fe8b20f3eb708ac4a629527b2190a
ed4354 <el478704@gmail.com>
2019-05-07 22:11:41 -0700

    header for sidemenu page

--- src/pages/Sample.js
+++ src/pages/Sample.js
@@ -1,6 +1,6 @@
 import SideMenu from 'react-native-side-menu';
 import React, {Component} from 'react'
-import {Text, View} from 'react-native'
+import {Text, View, StyleSheet} from 'react-native'
 import Menu from './Menu'
 import LinearGradient from "react-native-linear-gradient";
 
@@ -44,16 +44,38 @@ class ContentView extends React.Component {
 }
 
 export default class Application extends React.Component {
+  static navigationOptions = ({ navigation }) => {
 
+    const { params = {} } = navigation.state
+    return {
+      headerTransparent: true,
+      headerBackground: <LinearGradient
+        start={{
+          x: -0.01,
+          y: 0.51,
+        }}
+        end={{
+          x: 1.01,
+          y: 0.49,
+        }}
+        locations={[0, 1]}
+        colors={["rgb(247, 132, 98)", "rgb(139, 27, 140)"]}
+        style={styles.navigationBarGradient}/>,
+      title: "PesoPay",
+      headerTintColor: "white",
+      headerLeft: null,
+      headerRight: null,
+      headerStyle: {
+      },
+    }
+  }
   onMenuItemSelected = item =>
     this.setState({
       isOpen: false,
       selectedItem: item,
     });
-
   render() {
     const menu = <Menu onItemSelected={this.onMenuItemSelected} />;
-
     return (
       <SideMenu
         menu={menu}
@@ -64,3 +86,9 @@ export default class Application extends React.Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  navigationBarGradient: {
+    flex: 1,
+  },
+})
